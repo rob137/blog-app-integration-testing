47ad709b10e85f8bebc8cc745086714ca6e53091
Robert Kirby <robertaxelkirby@gmail.com>
2018-01-16 11:51:53 +0000

    added integration tests & dependencies

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
@@ -11,7 +10,6 @@ const { BlogPost } = require('./models');
 
 const app = express();
 
-app.use(morgan('common'));
 app.use(bodyParser.json());
 
 app.get('/posts', (req, res) => {
